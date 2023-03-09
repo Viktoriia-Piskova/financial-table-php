bc25c5e3c4860ba8a1b21fd22bd26e9a8010c786
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2023-03-09 19:45:50 +0200

    initial commit

---
+++ app/App.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+function getTransactionFiles(): array
+{
+    $files = [];
+
+    foreach (scandir(FILES_PATH) as $file) {
+        echo '<pre>' . var_dump($file) . '</pre>';
+    }
+
+    return $files;
+}
---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+
+require APP_PATH . "App.php";
+
+getTransactionFiles();
+?>
+
+
+/* YOUR CODE (Instructions in README.md) */
